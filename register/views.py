b56718c8f8d313498a789d2a6731f8fdba69a057
Frank William Daniels <frankwd@stud.ntnu.no>
2022-06-15 13:49:01 +0200

    Fixed logo for all pages and removed inheritance for login/registration

--- register/views.py
+++ register/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from pip import main
+
 from .forms import RegisterForm
-from main.models import Theme
+from main.models import Theme, Logo
 from django.contrib.auth import *
 
 # Create your views here.
 
+def login(response):
+    logo = Logo.objects.filter(pk=1).get()
 
 def register(response):
+    logo = Logo.objects.filter(pk=1).get()
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
@@ -16,4 +20,4 @@ def register(response):
     else:
         form = RegisterForm()
 
-    return render(response, "register/register.html", {"form": form})
+    return render(response, "register/register.html", {"form": form, "logo": logo})
